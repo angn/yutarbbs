a9b371e3ccbf44d945f23056ab28bcfcffdae845
holies <para.keon@gmail.com>
2012-10-29 14:33:54 +0900

    자동 수정 방지

--- app/index.php
+++ app/index.php
@@ -5,7 +5,7 @@ if ($my)
 ?>
 <?php if (!$my): ?>
 <form name=login action="<?= u('gateway') ?>" method=post class=login><p>
-<label><span>아이디</span> <input type=text name=userid autofocus placeholder="아이디"></label>
+<label><span>아이디</span> <input type=text name=userid autofocus placeholder="아이디" autocorrect=off autocapitalize=off></label>
 <label><span>암호</span> <input type=password name=passwd placeholder="암호"></label>
 <input type="submit" value="로그인&raquo;">
 <label><input type=checkbox name=persistent>로그인 유지</label>
