5327adad05a18ab6139eb05ea55da660c11944e8
holies <para.keon@gmail.com>
2012-12-22 16:55:12 +0900

    Improve database performance

--- lib/yutarbbs/database.rb
+++ lib/yutarbbs/database.rb
@@ -1,11 +1,22 @@
 require 'mysql'
 
 module Yutarbbs::Database
-  def my
+  def connect
     my = Mysql.connect '127.0.0.1', 'yutar', '', 'yutar'
     my.charset = 'utf8'
     my.query "SET time_zone='+09:00'"
     my
+  end
+  
+  def my
+    @@my ||= connect
+  end
+  
+  def query query, *params
+    my.prepare(query).execute(*params)
+  rescue Mysql::ClientError::ServerGoneError
+    @@my = nil
+    retry
   end
 
   def with_sym_keys hash
@@ -13,29 +24,32 @@ module Yutarbbs::Database
   end
 
   def fetch_one query, *params
-    with_sym_keys my.prepare(query).execute(*params).fetch_hash
+    with_sym_keys query(query, *params).fetch_hash
   end
 
   def fetch_all query, *params
     result = []
-    my.prepare(query).execute(*params).each_hash do |e|
+    query(query, *params).each_hash do |e|
       result << with_sym_keys(e)
     end
     result
   end
 
   def insert table_name, data
-    my.prepare("INSERT INTO #{table_name} (#{data.keys * ','}) VALUES (#{%w/?/ * data.values.length * ','})").execute(*data.values).insert_id
+    query("INSERT INTO #{table_name} (#{data.keys * ','}) VALUES (#{%w/?/ * data.values.length * ','})", *data.values).insert_id
   end
 
   def update table_name, where, *params, data
-    sets = data.keys.map { |e| "#{e}=?" } * ','
-    puts "UPDATE #{table_name} SET #{sets} WHERE #{where}"
-    my.prepare("UPDATE #{table_name} SET #{sets} WHERE #{where}").execute *data.values, *params
+    if data.is_a? Hash
+      sets = data.keys.map { |e| "#{e}=?" } * ','
+      query "UPDATE #{table_name} SET #{sets} WHERE #{where}", *data.values, *params
+    else
+      query "UPDATE #{table_name} SET #{data} WHERE #{where}", *params
+    end
   end
 
   def delete table_name, where, *params
-    my.prepare("DELETE FROM #{table_name} WHERE #{where}").execute *params
+    query "DELETE FROM #{table_name} WHERE #{where}", *params
   end
 
   def now
